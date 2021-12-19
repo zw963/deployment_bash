5ce9be6d5df57031eddd0581aa644d9ad01510e3
Billy.Zheng <vil963@gmail.com>
2021-12-19 16:49:25 +0800

    Add sysctl_for_proxy function.

--- deploy_start.sh
+++ deploy_start.sh
@@ -742,6 +742,45 @@ esac
     done
 }
 
+function sysctl_for_proxy () {
+    cat <<'HEREDOC' > /etc/sysctl.d/99-proxy.conf
+fs.file-max=818354
+
+net.core.rmem_max=67108864
+net.core.wmem_max=67108864
+net.core.netdev_max_backlog=250000
+net.core.somaxconn=4096
+
+net.ipv4.ip_forward=1
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_fin_timeout=30
+net.ipv4.tcp_keepalive_time=1200
+net.ipv4.ip_local_port_range=10000 65000
+net.ipv4.tcp_max_syn_backlog=8192
+net.ipv4.tcp_max_tw_buckets=5000
+net.ipv4.tcp_mem=25600 51200 102400
+net.ipv4.tcp_rmem=4096 87380 67108864
+net.ipv4.tcp_wmem=4096 65536 67108864
+net.ipv4.tcp_mtu_probing=1
+
+# 开启内核 fastopen, Linux 3.7 以上支持, 3.13 才默认开启.
+# 等价于 echo 3 > /proc/sys/net/ipv4/tcp_fastopen
+net.ipv4.tcp_fastopen=3
+HEREDOC
+
+    if kernel_version_greater_than 4.9 && modprobe tcp_bbr && lsmod | grep bbr; then
+        echo "net.core.default_qdisc=fq" >> /etc/sysctl.d/99-proxy.conf
+        echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.d/99-proxy.conf
+    fi
+
+    sysctl -p > /dev/null
+
+    # test bbr is enabled
+    sysctl net.ipv4.tcp_available_congestion_control |grep bbr
+    sysctl -n net.ipv4.tcp_congestion_control |grep bbr
+}
+
 function is_listen () {
     local port=$*
     netstat -tunl |fgrep 'LISTEN' |awk '{print $4}' |grep ":${port}$"
