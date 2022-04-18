a6822b9687f9ee945041efc65167bead2920ef01
Billy.Zheng <vil963@gmail.com>
2022-04-18 20:59:25 +0800

    Add nginx ssl config when deploy tls

--- deploy_start.sh
+++ deploy_start.sh
@@ -1045,6 +1045,29 @@ function deploy_tls () {
                        --reloadcmd "$reload_command"
 
     if [[ $(stat -c%s /etc/ssl/$domain_name/fullchain.pem) -ge 5000 ]]; then
+        mkdir -p /etc/nginx/conf.d
+        cat <<HEREDOC > /etc/nginx/conf.d/${domain_name}.conf
+server {
+    listen 443 ssl;
+    ssl_certificate /etc/ssl/$domain_name/fullchain.pem; # use fullchain.cer for complete certificate
+    ssl_certificate_key /etc/ssl/$domain_name/privkey.pem; # keep this private as much as possible
+
+    ssl_session_cache shared:SSL:20m;
+    ssl_session_timeout 60m;
+    ssl_prefer_server_ciphers on;
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+
+    ## Enable below if you will follow `Improve Security` below
+    ## https://gist.github.com/dorelljames/ae589f52f735949e3b678b6191804c59#improve-security
+
+    # ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
+    # ssl_dhparam /etc/ssl/certs/dhparams.pem;
+    # add_header Strict-Transport-Security max-age=31536000;
+}
+HEREDOC
+
+        systemctl restart nginx
+
         echo "Certificate install to \`[0m[0;33m/etc/ssl/$domain_name/fullchain.pem[0m', \`[0m[0;33m/etc/ssl/$domain_name/privkey.pem[0m'"
     else
         echo 'Install certificate failed?'
